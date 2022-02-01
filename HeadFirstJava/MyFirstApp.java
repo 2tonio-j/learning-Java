dcf21210e4cd6ef299b22c54d0e28c02fcfec8a0
2tonio-j <justjones1410@gmail.com>
2022-02-01 00:13:01 -0500

    updated MyFirstApp with loops and declarations

--- HeadFirstJava/MyFirstApp.java
+++ HeadFirstJava/MyFirstApp.java
@@ -1,11 +1,52 @@
-public class MyFirstApp { 
-  
+//This is my first Java program; see HeadFirst, Chp1	
+
+public class MyFirstApp {
+
 	public static void main (String[] args) {
     		System.out.println("I Rule");
 		System.out.println("The World");
+
+
+		// print a new line using 'println'
+		System.out.println();
+
+
+		// declarations & assignments 	
+		int x = 3;
+		String name = "Dirk";
+		x = x * 17;
+		// call methods
+		System.out.println("x is " + x);
+		double d = Math.random();
+		System.out.println("d is " + d);
+
+
+		// print a new line using platform-independent way (%n)
+		System.out.printf("%nA new line was printed before this one;%nand a new line comes after this%n");
+
+
+		// For and While Loops
+		while (x > 49) { 
+			x = x - 1;
+			System.out.println("right now x is " + x);
+		}
+			
+		for (int j = 0; j < 4; j = ++j) {
+			System.out.print("and now x is " + x);
+		}
+
+
+		// IF-ELSE conditions
+		if (x == 0) {
+			System.out.println("x must be 10");
+		} else {
+			System.out.println("x isn't 10");
+		}
+		if ((x < 3) & (name.equals("Dirk"))) {
+			System.out.print("Gently");
+		}
+
+		System.out.printf("this line runs no matter what%n");
+		
 	}
 }
-
-
-
-
